71379d9465689bd57c275732f745c1ac4731eae1
timekeeper01 <46344493+timekeeper01@users.noreply.github.com>
2019-04-13 22:19:45 +0800

    Update MapTest.java

--- src/mapDemo/MapTest.java
+++ src/mapDemo/MapTest.java
@@ -6,24 +6,24 @@ import java.util.Set;
 import java.util.TreeMap;
 
 /*
- * �����ַ���"fdgfdnkughiueskldm"
- * ��ȡ�ַ�����ÿ����ĸ���ֵĴ�������ʽa(2)b(0)c(2)..
+ * 给定字符串"ni..usazvcnbccnosewa"
+ * 获取字符串中每个字母出现的次数，格式a(2)b(0)c(2)..
  */
 /*
- * �ܴ洢ӳ���ϵ�������������map����(set��Ȼ��֤��Ψһ�ԣ����ǲ��ܴ洢ӳ���ϵ)
- * ��ϵһ�������������ţ�ʹ��map�������Ա�֤Ψһ�Ե�һ���߱���˳��abc
- * ʹ��treeset
+ * 能存储映射关系的容器有数组和map集合(set虽然保证了唯一性，但是不能存储映射关系)
+ * 关系一方不存在有序编号，使用map，还可以保证唯一性的一方具备着顺序abc
+ * 使用treeset
  * 
- * �������մ洢��Ӧ������ĸ�ʹ�����Ӧ��ϵ
+ * 集合最终存储的应该是字母和次数对应关系
  * 
- * 1.�Ƚ��ַ�������ַ�����
- * 2.�����ַ����飬��ÿһ����ĸ��Ϊ��ȥ��map����
- * 	�����ĸ�����ڣ��ͽ�����ĸ��Ϊ����1��Ϊֵ�洢��map������
- * 	������ڣ��ͽ�����ĸ����Ӧֵȡ����+1���ٽ�����ĸ��+1���ֵ�洢��map������
- * 	����ͬ��ֵ�Ḳ�ǣ������ͼ�¼�˸���ĸ�ĳ��ִ���
+ * 1.先将字符串变成字符数组
+ * 2.遍历字符数组，用每一个字母作为键去查map集合
+ * 	如果字母不存在，就将该字母作为键，1作为值存储到map集合中
+ * 	如果存在，就将该字母键对应值取出并+1，再将该字母和+1后的值存储到map集合中
+ * 	键相同，值会覆盖，这样就记录了该字母的出现次数
  */
 
-//���ü���ֵ���������Ϊnull��˵�������������
+//利用键查值，如果返回为null，说明不存在这个键
 
 
 public class MapTest {
@@ -40,16 +40,16 @@ public class MapTest {
 		for (int i = 0; i < chs.length; i++) {
 			if(!(chs[i]>='a'&&chs[i]<='z'||chs[i]>='a'&&chs[i]<='z'))
 				continue;
-			//������������ĸ��Ϊ��ȥ�������ȡ��
+			//将遍历到的字母作为键去查表，获取表
 			Integer value=map.get(chs[i]);
-//			if(value==null)//���û�����ֵ
+//			if(value==null)//如果没有这个值
 //				map.put(chs[i], 1);
-//			else {//��������ֵ
+//			else {//如果有这个值
 //				value++;
 //				map.put(chs[i], value);
 //			}
 			
-			int count=0;//���ڼ�¼����
+			int count=0;//用于记录次数
 			if(value!=null) {
 				count=value;
 			}
@@ -60,15 +60,15 @@ public class MapTest {
 		return toString(map);
 	}
 
-	//��map�����е�Ԫ��ת��ָ����ʽ���ַ���
+	//将map集合中的元素转成指定格式的字符串
 	private static String toString(Map<Character, Integer> map) {
-		//���ݶ࣬�������Ͷ�Ҫ����ַ�����ʹ��StringBUilder
+		//数据多，无论类型都要变成字符串，使用StringBUilder
 		StringBuilder sb=new StringBuilder();
 		Set<Character> keyset=map.keySet();
 		for (Iterator<Character> it = keyset.iterator(); it.hasNext();) {
 			Character key =  it.next();
 			Integer value=map.get(key);
-			//��ֵ�洢��sb��
+			//将值存储到sb中
 			sb.append(key+"("+value+")");	
 		}
 		return sb.toString();
